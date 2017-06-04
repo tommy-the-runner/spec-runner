2ac761d763b7e39ae255b5da47eed2c3d49f8583
Mateusz Derks <mateusz.derks@toptal.com>
2017-06-04 12:42:20 +0200

    Support configurable subject package name

---
+++ fixtures/spec1a.js
@@ -0,0 +1,24 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const sum = require('your code here')
+
+describe('my function', function () {
+    it('should return sum of positive numbers', function () {
+        const total = sum(3, 5)
+
+        expect(total).to.equal(8)
+    })
+
+    it('should return sum of negative numbers', function () {
+        const total = sum(-3, -5)
+
+        expect(total).to.equal(-8)
+    })
+
+    it('should accept 0 as neutral operand', function () {
+        const total = sum(0, 3)
+
+        expect(total).to.equal(3)
+    })
+})
